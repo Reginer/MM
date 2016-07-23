e940241a90e8883e126a6a17014ed6adfe30541d
Reginer <282921012@qq.com>
2016-07-23 22:20:49 +0800

    Initial commit

---
+++ app/src/main/java/com/jason/mm/AppJason.java
@@ -0,0 +1,54 @@
+package com.jason.mm;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+
+/**
+ * ----------Dragon be here!----------/
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑
+ * 　　　　┃　　　┃代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━
+ *
+ * @author Reginer on  2016/7/23 21:17.
+ *         Description:Application
+ */
+public class AppJason extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initHX();
+    }
+
+    /**
+     * 初始化环信
+     */
+    private void initHX() {
+
+        EMOptions options = new EMOptions();
+// 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+//初始化
+        EMClient.getInstance().init(this, options);
+//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+    }
+}
---
+++ app/src/main/java/com/jason/mm/MainActivity.java
@@ -0,0 +1,55 @@
+package com.jason.mm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        register();
+    }
+
+    private void register() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    EMClient.getInstance().createAccount("Reginer", "123456");
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            int errorCode = e.getErrorCode();
+                            if (errorCode == EMError.NETWORK_ERROR) {
+                                Toast.makeText(MainActivity.this, "网络异常，请检查网络！", Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_ALREADY_EXIST) {
+                                Toast.makeText(MainActivity.this, "用户已存在！", Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_AUTHENTICATION_FAILED) {
+                                Toast.makeText(MainActivity.this, "注册失败，无权限！", Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_ILLEGAL_ARGUMENT) {
+                                Toast.makeText(MainActivity.this, "用户名不合法", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "注册失败", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        }).start();
+    }
+}
